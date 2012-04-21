7663fea5cb4824bf16e6bef7ecf0936bf17fa67d
unknownexception <unknownliveid@hotmail.com>
2012-04-21 12:06:29 +0700

    corpus reader

--- postagger/Corpus.fs
+++ postagger/Corpus.fs
@@ -2,13 +2,14 @@
 
 open System.Collections.Generic
 
-type TaggedWord = string * string
+type TaggedWord = string * string 
+
 
 type CorpusData = {
-    Lexicon:Map<string, Map<string, int>>;
-    Unigrams:Map<string, int>;
-    Bigrams:Map<string, int>;
-    Trigrams:Map<string, int>
+    Lexicon:Dictionary<string, int>;
+    Unigrams:Dictionary<string, int>;
+    Bigrams:Dictionary<string, int>;
+    Trigrams:Dictionary<string, int>
     }
 
 // read and parse corpus xml file
@@ -16,6 +17,9 @@ type CorpusData = {
 // http://opencorpora.org/?page=downloads
 // http://opencorpora.org/dict.php?act=gram
 
+let delimiter = " "
+
+let inline taggedToStr word =  fst word + delimiter + snd word
 
 let inline readTags (xmlReader:System.Xml.XmlReader) = 
     seq {
@@ -24,44 +28,55 @@ let inline readTags (xmlReader:System.Xml.XmlReader) =
     } |> Seq.fold (fun accum x  -> accum + "," + x) ""
 
 let inline readSentence (xmlReader:System.Xml.XmlReader) =     
-    seq {
+    [
         yield "<S>", "<S>"
         while xmlReader.ReadToFollowing("token") do    
             let a = xmlReader.GetAttribute("text") 
             xmlReader.ReadToFollowing("l") |> ignore       
             yield a, readTags (xmlReader.ReadSubtree())
         yield "</S>", "</S>"
-        } 
+     ]
 
 let inline incDict (dict:Dictionary<'a, int>, key:'a) = 
     if dict.ContainsKey(key) then         
         dict.Item key <- dict.Item key + 1
     else
         dict.Add(key, 1)
+    dict
     
 
-let handleSentence (sentence:seq<TaggedWord>, corpus_data:CorpusData):CorpusData = 
-    let handleWord tagged_word i = 
-        //add lexiconentry
-        corpus_data.Lexicon |> Map.filter (fun x y -> x) 
-        {Lexicon = incDict corpus_data.Lexicon.Add(tagged_word); 
-         Unigrams = incDict corpus_data.Unigrams tagged_word; 
-         Bigrams = new Dictionary<string, int>(); 
-         Trigrams = new Dictionary<string, int>}
+let handleSentence (sentence:TaggedWord list, corpus_data:CorpusData):CorpusData = 
+    
+    let rec handleWord (c:CorpusData,i:int) =
+        let getBigrams = 
+            if (i > 0) then
+                incDict (c.Bigrams, ( (snd sentence.[i - 1]) + delimiter + (snd sentence.[i]) ));
+            else 
+                c.Bigrams
+        let getTrigrams = 
+            if (i > 1) then
+                incDict (c.Trigrams, ( (snd sentence.[i - 2]) + delimiter + (snd sentence.[i - 1]) + delimiter + (snd sentence.[i]) ));
+            else 
+                c.Trigrams
+        if i = sentence.Length - 1 then
+            c
+        else            
+            let corpusd = {Lexicon = incDict (c.Lexicon, taggedToStr sentence.[i]); Unigrams =  incDict (c.Unigrams, (snd sentence.[i])); Bigrams =  getBigrams; Trigrams = getTrigrams;}
+            handleWord (corpusd , i+1)  
 
-    sentence |> Seq.mapi (fun x i -> handleWord x i)
+    handleWord (corpus_data, 0)
 
-let readCorpus (input_stream:System.IO.Stream, handler) =    
+let readCorpus (input_stream:System.IO.Stream) =    
    
     use xmlReader = System.Xml.XmlReader.Create(input_stream)   
     seq {
         while xmlReader.ReadToFollowing("sentence") do
             yield xmlReader.ReadSubtree()
-        } |> Seq.fold (fun corpus_data sentence_tree -> handleSentence ((readSentence sentence_tree) corpus_data))
-                {Lexicon = Map.empty; 
-                Unigrams = Map.empty; 
-                Bigrams = Map.empty; 
-                Trigrams = Map.empty}
+        } |> Seq.fold (fun corpus_data sentence_tree -> handleSentence ((readSentence sentence_tree), corpus_data))
+                {Lexicon =  new Dictionary<string, int>(); 
+                Unigrams =  new Dictionary<string, int>(); 
+                Bigrams =  new Dictionary<string, int>(); 
+                Trigrams =  new Dictionary<string, int>()}
 
 
 //
--- postagger/Model.fs
+++ postagger/Model.fs
@@ -1,2 +1,12 @@
 ﻿module TinyNLP.POST.Model
 
+open TinyNLP.POST.Corpus
+
+let inline processTrigram index trigram = 
+    let t1t2t3 = string (fst trigram)
+    let t1t2 = (t1t2t3.Split [|delimiter|]).[0] + " " + (t1t2t3.Split [|' '|]).[1]
+    printfn "%A" t1t2t3
+    
+let calculateLambdas (corpus:CorpusData) = 
+    let corpus_size = Seq.fold (fun accum x -> accum + x) 0 corpus.Unigrams.Values 
+    corpus.Trigrams |> Seq.mapi processTrigram
